4ffa6ef3025d43731201bbc658658af25319ae67
mohamed khairy <mohamed khairy>
2024-08-24 08:01:42 +0300

    setting the enviroment

---
+++ src/main.js
@@ -0,0 +1,18 @@
+import kaplay from 'kaplay';
+
+const k = kaplay({
+  width: 1280,
+  height: 720,
+  letterboz: true,
+  global: false,
+  scale: 2,
+});
+
+k.loadSprite('kriby', './kriby.png');
+k.loadSprite('obstacles', './obstacles.png');
+k.loadSprite('background', './background.png');
+k.loadSprite('clouds', './clouds.png');
+
+k.loadSound('jump', './jump.wav');
+k.loadSound('hurt', './hurt.wav');
+k.loadSound('confirm', './confirm.wav');
