85e43d368c2ccd3ca8f66af8fd3170b12640a427
mohamed khairy <mohamed khairy>
2024-08-25 08:15:10 +0300

    added the background and the clouds and the movement of it and added the obsteclase

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
+import { appWindow } from '@tauri-apps/api/window';
 import kaplay from 'kaplay';
+import { makeBackground } from './utils';
+import { SCALE_FACTOR } from './constants';
 
 const k = kaplay({
   width: 1280,
   height: 720,
-  letterboz: true,
+  letterbox: true,
   global: false,
   scale: 2,
 });
@@ -16,3 +19,45 @@ k.loadSprite('clouds', './clouds.png');
 k.loadSound('jump', './jump.wav');
 k.loadSound('hurt', './hurt.wav');
 k.loadSound('confirm', './confirm.wav');
+
+addEventListener('keydown', async (key) => {
+  if (key.code === 'F11') {
+    if (await appWindow.isFullscreen()) {
+      await appWindow.setFullscreen(false);
+      return;
+    }
+
+    appWindow.setFullscreen(true);
+  }
+});
+
+k.scene('start', async () => {
+  makeBackground(k);
+
+  const map = k.add([
+    k.sprite('background'),
+    k.pos(0, 0),
+    k.scale(SCALE_FACTOR),
+  ]);
+
+  const clouds = map.add([
+    k.sprite('clouds'),
+    k.pos(),
+    {
+      speed: 5,
+    },
+  ]);
+
+  clouds.onUpdate(() => {
+    clouds.move(clouds.speed, 0);
+    if (clouds.pos.x > 700) {
+      clouds.pos.x = -500;
+    }
+  });
+
+  map.add([k.sprite('obstacles'), k.pos()]);
+});
+
+k.scene('main', async () => {});
+
+k.go('start');
